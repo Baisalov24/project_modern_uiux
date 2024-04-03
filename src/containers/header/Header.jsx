654aaea750ddf099f35cb54be53159bcc3cdc7d8
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-03 07:09:24 +0600

    header bug fixed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-// import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
-import './header.css';
+import React from "react";
+import ai from "../../assets/ai.png";
+import "./header.css";
 
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with us</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Turnkey Web Application Development</h1>
+      <p>
+        Transforming Your Ideas Into Reality With Cutting-Edge Technology. Our
+        team leverages the latest technologies to create scalable and dynamic
+        web applications. Let us help you innovate, engage, and grow
+      </p>
 
       <div className="gpt3__header-content__input">
-        {/* <input type="email" placeholder="Your Email Address" /> */}
-        <button type="button">Get In Touch</button>
+        <button type="button">
+          <a href="#contactUs">Get In Touch</a>{" "}
+        </button>
       </div>
-
-      {/* <div className="gpt3__header-content__people">
-        <img src={people} />
-        <p>1,600 people requested access a visit in last 24 hours</p>
-      </div> */}
     </div>
 
     <div className="gpt3__header-image">
