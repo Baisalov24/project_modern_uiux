deae03679b58320b4f757aa2594eb31398d91263
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-04 12:12:27 +0600

    portfolio fixed

--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -2,36 +2,36 @@ import React from "react";
 import "./portfolio.css";
 import project_1 from '../../assets/project_1.png'
 import project_2 from '../../assets/project_2.png'
-// import project_3 from '../../assets/project_3.png'
+import project_3 from '../../assets/project_3.png'
 
 
 const Portfolio = () => {
   return (
     <section className="third-section" id="portfolio">
-      <h1 className="gradient__text portfolio_title">Our last projects</h1>
+      <h1 className="gradient__text portfolio_title">Website Templates</h1>
       <h2 className="gradient__text portfolio_subtitle">
-        This is the subtitle.
+      Discover Modern Websites
       </h2>
       <div class="blocks-container">
         <div class="block">
           <img src={project_1} alt="Image 1" />
           <p>Photo description 1</p>
-          <a href="link1" class="button">
-            Click
+          <a href="https://yoga-studio-website.vercel.app/" class="button">
+          Visit
           </a>
         </div>
         <div class="block">
           <img src={project_2} alt="Image 2" />
-          <p>hoto description 2</p>
+          <p>photo description 2</p>
           <a href="link2" class="button">
-          Click
+          Visit
           </a>
         </div>
         <div class="block">
-          <img src={project_2} alt="Image 3" />
-          <p>hoto description 3</p>
-          <a href="link3" class="button">
-          Click
+          <img src={project_3} alt="Image 3" />
+          <p>photo description 3</p>
+          <a href="https://headphone-maket.vercel.app/" class="button">
+          Visit
           </a>
         </div>
       </div>
